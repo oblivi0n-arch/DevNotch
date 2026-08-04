8bee2f91d1d01a027352c4e70e9c7295986e6aa2
oblivi0n-arch <jakub.deck8@gmail.com>
2026-08-04 20:01:46 +0200

    Initial Commit

---
+++ DevNotch/DevNotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevNotchApp.swift
+//  DevNotch
+//
+//  Created by Jakub Majcher on 04/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct DevNotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
